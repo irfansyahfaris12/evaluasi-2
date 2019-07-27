20a5ee1f50cd5aa8e7343e9b7ba812664b6ef0e5
irfansyahfaris12 <52726995+irfansyahfaris12@users.noreply.github.com>
2019-07-27 14:54:27 +0700

    Update evaluasi-3.js

--- evaluasi-3.js
+++ evaluasi-3.js
@@ -6,7 +6,7 @@ document.write('<br>')
 var capitalize = ['hello world']
 
 var capital = capitalize.map(function(string) {
-    return string.capital(upper)
+    return string.capital(toUpperCase())
 });
 
 document.write(capital)
